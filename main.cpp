3f7d2873795a3de1a42b8650031b514b4cd9a212
mo-ayman <m.ayman.saaed@gmail.com>
2023-12-03 17:38:30 +0200

    add code to use nfa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <unordered_map>
 
 #include "LexicalRules/RuleParser.h"
 #include "LexicalRules/RuleTree.h"
+#include "NFA/NFAConstruction.h"
 
 int main() {
     // get dir path of the source code and concatenate with the file name
@@ -9,13 +11,20 @@ int main() {
     example_path = example_path.substr(0, example_path.find_last_of("\\/"));
     example_path += "/example.txt";
     const std::vector<RuleTree *> rules = RuleParser::parseFromFile(example_path);
+    auto* nfaConstruction = new NFAConstruction();
+    nfaConstruction->constructNFA(rules);
+    std::vector<std::map<char, std::vector<int>>> nfs = nfaConstruction->getNfs();
+    std::unordered_map<int, std::tuple<std::string, Priority, int>> finalStates = nfaConstruction->getFinalStates();
+    int startStateIndex = nfaConstruction->getStartStateIndex();
 
+//    nfaConstruction->print();
     // print all rules
     for (const auto& rule: rules) {
         rule->print();
     }
 
     std::cout << std::endl;
+
     return 0;
 }
 
