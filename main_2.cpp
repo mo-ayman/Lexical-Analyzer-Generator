fc54ffe534b3763f46172ec356b3d0c985ea56c0
3amo-magdy <amoramagdymahmoud@gmail.com>
2023-12-08 13:37:46 +0200

    NEEDS REVISION (HELP)
    doesn't work with linux i think :(

--- main_2.cpp
+++ main_2.cpp
@@ -12,8 +12,11 @@ int main(const int argc, char** argv) {
     const auto dfa_path = std::string(argv[1]);
     const auto input_path = std::string(argv[2]);
     const auto output_path = std::string(argv[3]);
+    const auto tokens_path = output_path.substr(0, output_path.find_last_of("\\/")+1) + "output_tokens.txt";
 
     std::ofstream outputFile(output_path);
+    std::ofstream tokensFile(tokens_path);
+
     LexicalAnalyzer lexer(input_path, 1024, dfa_path);
 
     Token token;
@@ -28,15 +31,18 @@ int main(const int argc, char** argv) {
                     outputFile << ", ";
                 }
                 k++;
+                tokensFile << "Error" << std::endl;
             }
             outputFile << std::endl;
         }
+        tokensFile << token.type << std::endl;
         outputFile << std::setw(8) << "Pos: " << std::setw(5) << token.filePos
                     << " | Type: " << std::setw(10) << token.type
                     << " | Lexeme: \"" << token.lexeme << "\""
                     << std::endl;
     } while (token.type != "EOF");
 
+    tokensFile.close();
     outputFile.close();
     return 0;
 }
