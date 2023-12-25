0d5748bf1c6454154426b6010795b469d45961e9
3amo-magdy <amoramagdymahmoud@gmail.com>
2023-12-26 00:14:02 +0200

    feat: exporting & importing the parsing table

---
+++ ParserGenerator/Main/parserGeneration.cpp
@@ -0,0 +1,123 @@
+#include <sstream>
+#include <iomanip>
+
+#include "../ParsingCFG/ParsingCFG.h"
+#include "../Parser/Parser.h"
+
+#include "../Predictive_PT/PPT.h"
+
+#include "../FirstAndFollow/First.h"
+#include "../FirstAndFollow/Follow.h"
+
+using namespace std;
+
+static void printTable(const std::unordered_map<Definition*, std::unordered_map<std::string, std::vector<Definition*>>>& table) {
+    // Print header
+    std::cout << std::setw(20) << "Non-terminal" << std::setw(20) << "Productions" << std::endl;
+    std::cout << std::setw(70) << std::setfill('-') << "" << std::setfill(' ') << std::endl;
+
+    // Print table content
+    for (const auto& entry : table) {
+        const auto nonTerminal = entry.first;
+        std::cout << std::setw(20) << nonTerminal->getName() << (nonTerminal->getIsTerminal() ? " T" : " NT") << std::endl;
+        for (const auto& innerEntry : entry.second) {
+            std::cout << std::setw(40) << innerEntry.first << " -> ";
+            for (const auto& item : innerEntry.second) {
+                std::cout << item->getName() << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::setw(70) << std::setfill('-') << "" << std::setfill(' ') << std::endl;
+    }
+}
+
+static std::string serializeTable(unordered_map<Definition *, unordered_map<string, vector<Definition *>>>& table, const Definition* start) {
+    std:: string result;
+    result += start->getName() + "#" + "0";
+    for (auto& entry: table) {
+        result += "\n";
+        auto d = entry.first;
+        result += d->getName() + "#";
+        result += d->getIsTerminal()? "1" : "0";
+        for (auto& [fst, snd]: entry.second) {
+            result += "\x1D";
+            result += fst; // string
+            for (const auto& item: snd) {
+                result += '\x1E';
+                result += item->getName() + "#";
+                result += item->getIsTerminal()? "1" : "0";
+            }
+        }
+    }
+    return result;
+}
+
+int main(const int argc, char** argv) {
+    // Get dir path of the source code and concatenate with the file name
+    std::string src_path = __FILE__;
+    src_path = src_path.substr(0, src_path.find_last_of("\\/"));
+    std::cout << src_path << std::endl;
+
+    const auto rules_path = src_path + "\\example_rules.txt"; // std::string(argv[1]);
+    std::cout<<src_path<<endl;
+
+    Definition* start;
+    const map<Definition *, std::vector<std::vector<Definition *>>> rules = ParsingCFG::parseFromFile(rules_path, &start);
+    // Print all rules
+    std::cout << "====================== Rules ======================" << std::endl;
+    for (const auto& rule: rules) {
+        std::cout << rule.first->getName() << " -> ";
+        for (const auto& alternative: rule.second) {
+            for (const auto& definition: alternative) {
+                if (definition->getIsTerminal()) {
+                    std::cout << "'" << definition->getName() << "' ";
+                } else {
+                    std::cout << definition->getName() << " ";
+                }
+            }
+            if (alternative != rule.second.back()) {
+                std::cout << "| ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+
+    auto* first = new First(rules);
+    first->constructFirst();
+    first->print();
+
+    auto* follow = new Follow(rules, first);
+    follow->constructFollow();
+    follow->print();
+
+    /*
+     * PPTable(Greatly part)
+     * */
+    map<Definition*, vector<pair<int, Definition*>>> first1 = first->getFirst();
+    map<Definition*, vector<Definition*>> follow2=follow->getFollow();//= new map<Definition*, vector<Definition*>>();
+    auto* obj=new PPT(rules,first1,follow2);
+    // Parsing table
+    auto table = *(obj->computePPT());
+    std::cout << "====================== PPTable ======================" << std::endl;
+    printTable(table);
+
+    // Save the parsing table
+    std::string serializedTable = serializeTable(table, start);
+    std::string filepath = src_path + "/parsingTable.txt";   //TODO: output
+
+    // Open the file for writing
+    std::ofstream outputFile(filepath);
+
+    // Check if the file is open
+    if (!outputFile.is_open()) {
+        std::cerr << "Error opening the file for writing: " << filepath << std::endl;
+        return 1;
+    }
+    outputFile << serializedTable;
+    outputFile.close();
+
+    //TODO: Collect Heap Garbage (if any)
+    return 0;
+}
---
+++ ParserGenerator/Main/parsing.cpp
@@ -0,0 +1,139 @@
+#include <sstream>
+#include <string>
+#include "../Parser/Parser.h"
+#include <iomanip>
+
+static void printTable(const std::unordered_map<Definition*, std::unordered_map<std::string, std::vector<Definition*>>>& table) {
+    // Print header
+    std::cout << std::setw(20) << "Non-terminal" << std::setw(20) << "Productions" << std::endl;
+    std::cout << std::setw(70) << std::setfill('-') << "" << std::setfill(' ') << std::endl;
+
+    // Print table content
+    for (const auto& entry : table) {
+        const auto nonTerminal = entry.first;
+        std::cout << std::setw(20) << nonTerminal->getName() << (nonTerminal->getIsTerminal() ? " T" : " NT") << std::endl;
+        for (const auto& innerEntry : entry.second) {
+            std::cout << std::setw(40) << innerEntry.first << " -> ";
+            for (const auto& item : innerEntry.second) {
+                std::cout << item->getName() << " ";
+            }
+            std::cout << std::endl;
+        }
+    std::cout << std::setw(70) << std::setfill('-') << "" << std::setfill(' ') << std::endl;
+    }
+}
+
+static void deserializeTable(const std::string& input, std::unordered_map<Definition*, std::unordered_map<std::string, std::vector<Definition*>>>& table, Definition** start) {
+    std::istringstream stream(input);
+    std::string token;
+    unordered_map<string, Definition*> defPool;
+    bool obtainedStart = false;
+    while (std::getline(stream, token, '\n')) {
+        if (token.empty()) {
+            continue;
+        }
+
+        Definition* definition;
+        int i = 0;
+        string name;
+        while(token[i]!='#') {
+            name += token[i];
+            i++;
+        }
+        i++;
+        const bool isTerminal = token[i++] == '1';
+        if (name == Definition::EPSILON) {
+            definition = Definition::getEpsilon();
+        }
+        else if(name == Definition::DOLLAR) {
+            definition = Definition::getDollar();
+        }
+        else {
+            definition = (defPool.find(name) == defPool.end())? new Definition(name, isTerminal) : defPool[name];
+            defPool[name] = definition;
+        }
+        table[definition] = unordered_map<string, vector<Definition*>>();
+        if(!obtainedStart) {
+            obtainedStart = true;
+            *start = definition;
+            continue;
+        }
+        while (token[i] == '\x1D') {
+            string innerString;
+            i++;
+            while ( i<token.size() && token[i]!='\x1D' && token[i]!='\x1E') {
+                innerString += token[i];
+                i++;
+            }
+
+            table[definition][innerString] = std::vector<Definition*>();
+            while(i<token.size() && token[i]=='\x1E'){
+                Definition* innerDefinition;
+                string innerName;
+                i++;
+                while(i<token.size()-1 && token[i]!='#') {
+                    innerName += token[i];
+                    i++;
+                }
+                i++;
+                const bool innerIsTerminal = token[i++] == '1';
+                if (innerName == Definition::EPSILON) {
+                    innerDefinition = Definition::getEpsilon();
+                }
+                else if(innerName == Definition::DOLLAR) {
+                    innerDefinition = Definition::getDollar();
+                }
+                else {
+                    innerDefinition = (defPool.find(innerName) == defPool.end())? new Definition(innerName, innerIsTerminal) : defPool[innerName];
+                }
+                defPool[innerName] = innerDefinition;
+                table[definition][innerString].push_back(innerDefinition);
+            }
+        }
+    }
+}
+
+int main(const int argc, char** argv) {
+    // Get dir path of the source code
+    std::string src_path = __FILE__;
+    src_path = src_path.substr(0, src_path.find_last_of("\\/"));
+    std::cout << src_path << std::endl;
+
+    // Output Files
+    const std::string LeftMostDerivationFile = src_path + "\\LeftMostDerivation.txt";   //TODO output
+    const std::string ParseTreeGraphFile = src_path + "\\ParseTreeGraph";               //TODO output
+
+    // Lexical Analyzer Initialization
+    const std::string lexical_analyzer_dfa = src_path + "\\lexical_analyzer.dat";   // serialized DFA file path //TODO input
+    const std::string input_program = src_path + "\\input_program.txt";             // initial input file path  //TODO input
+    auto lex = LexicalAnalyzer(input_program, 1024,lexical_analyzer_dfa);
+
+    // Load parsing table
+    std::string filepath = src_path+"/ParsingTable.txt";     //TODO input
+    std::ifstream file(filepath);
+    if (!file.is_open()) {
+        std::cerr << "Error opening the file: " << filepath << std::endl;
+        return 1;
+    }
+    std::string serializedTable((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    cout<< serializedTable;
+
+    file.close();
+
+    std::unordered_map<Definition*, std::unordered_map<std::string, std::vector<Definition*>>> table;
+    Definition* start;
+    deserializeTable(serializedTable, table, &start);
+    printTable(table);
+
+    // Parser Initialization
+    auto parser = Parser(table);
+
+    // Generating Outputs
+    auto parseTreeRoot = *parser.parse(lex, start);
+    parseTreeRoot.plotGraph(ParseTreeGraphFile);
+
+    std::ofstream outputFile(LeftMostDerivationFile);
+    outputFile << parseTreeRoot;
+    outputFile.close();
+
+}
